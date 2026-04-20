f86e3917320420d54e24cfd261123ec365ca8b20
trazzgameofficiel-droid <trazzgameofficiel@gmail.com>
2026-04-20 23:23:24 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,19 +8,19 @@ TOKEN = os.environ.get("BOT_TOKEN", "8713104272:AAGuu-HE3v6_yIIL9dbIf7s49SpVj4oR
 # MENU PRINCIPAL
 def main_menu():
     keyboard = [
-        [InlineKeyboardButton(" Accéder à la mini app",
-            web_app=WebAppInfo(url="https://69e66bc5512ca700efd7a290--dainty-marzipan-b99def.netlify.app/")
+        [InlineKeyboardButton("📲 Accéder à la mini app",
+            web_app=WebAppInfo(url="https://69e63efbc84e202d194dccb0--dainty-marzipan-b99def.netlify.app/")
         )],
-        [InlineKeyboardButton(" Support", callback_data="support")],
-        [InlineKeyboardButton(" Canal", url="https://t.me/rsqcoffe")]
+        [InlineKeyboardButton("🛟 Support", callback_data="support")],
+        [InlineKeyboardButton("📢 Canal", url="https://t.me/rsqcoffe")]
     ]
     return InlineKeyboardMarkup(keyboard)
 
 # MENU SUPPORT
 def support_menu():
     keyboard = [
-        [InlineKeyboardButton(" Snapchat", url="https://www.snapchat.com/add/zerorisque6.2")],
-        [InlineKeyboardButton(" Retour", callback_data="menu")]
+        [InlineKeyboardButton("👻 Snapchat", url="https://www.snapchat.com/add/zerorisque6.2")],
+        [InlineKeyboardButton("🔙 Retour", callback_data="menu")]
     ]
     return InlineKeyboardMarkup(keyboard)
 
@@ -46,10 +46,9 @@ async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     if query.data == "support":
         await query.edit_message_caption(
-            caption="""
- Support
+            caption="""🛟 Support
 
-Contacte-nous rapidement via Snapchat 
+Contacte-nous rapidement via Snapchat 👻
 """,
             reply_markup=support_menu(),
             parse_mode="HTML"
